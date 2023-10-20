4fe0c5c1f357088cdb4b903919c0c5d3cb69cf0c
kwonup <kkwon1122@gmail.com>
2023-10-21 04:20:02 +0900

    hehe

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        setMode('WELCOME');
+        setMode('WELCOMEeeeeeeeeeeeeee');
       }}></Header>
       <Nav topics={topics} onChangeMode={(_id)=>{
         setMode('READ');
